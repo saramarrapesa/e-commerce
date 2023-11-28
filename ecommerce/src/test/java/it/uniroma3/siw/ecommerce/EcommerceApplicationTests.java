51fca67deb10de781224ffb8a93bbbc234eb69a8
saramarrapesa <115159885+saramarrapesa@users.noreply.github.com>
2023-11-28 18:25:58 +0100

    first commit
    ho configurato oauth2

---
+++ ecommerce/src/test/java/it/uniroma3/siw/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
